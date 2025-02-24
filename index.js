0d2e9c523dbbe92e8261c08db0af2a50f55937fb
Ravi Kumar Reddy <166979268+techabbayi@users.noreply.github.com>
2025-02-24 15:25:13 +0530

    Completed Assignment

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const { resolve } = require('path');
+const resolve  = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
 
+
+
+const DB_URL = 'mongodb+srv://lokeswara:techabbayi@cluster0.riph9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(DB_URL)
+.then(()=>{console.log('Mongodb connected Successfully!')})
+.catch(err => console.log('Mongodb connection failed : ',err));
+
+
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Node JS Server listening on http://localhost:${port}`);
 });
